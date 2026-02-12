6a74772f5008a6223fbc775ee61556bd777e77ef
pndlghn <alghanipandu@gmail.com>
2026-02-12 13:27:15 +0700

    Pixel Page Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import PortfolioHero from "../components/section/porfolio/PortfolioHero";
 import PortfolioGallery from "../components/section/porfolio/PortfolioGallery";
 
+import { useEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
+
 const Portfolio = () => {
+  useEffect(() => {
+    // Mengirim Standard Event 'ViewContent' dengan parameter tambahan
+    ReactPixel.track("ViewContent", {
+      content_name: "Portfolio Page",
+      content_category: "Work Samples",
+    });
+
+    // ATAU mengirim Custom Event (Nama bebas)
+    ReactPixel.trackCustom("ViewPortfolio", {
+      status: "User viewed portfolio list",
+    });
+  }, []);
+
   return (
     <div className="bg-black min-h-screen text-white">
       {/* Hero Section Portfolio */}
